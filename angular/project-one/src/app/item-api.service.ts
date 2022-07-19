2242cc621fe1bc7d2920dd8af784f9480a85c61e
ToddF456 <ToddF456@gmail.com>
2022-07-19 17:36:20 -0400

    Added to angular project
    Added basic find all method to the angular project, as well as changing where it is located in the folders.

---
+++ angular/project-one/src/app/item-api.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemApiService {
+
+  http :HttpClient;
+
+  constructor(http :HttpClient) 
+  {
+    this.http = http; 
+  }
+
+  findAll() :Observable<any>
+  {
+    return this.http.get(environment.apiUrl);
+  }
+
+}
+
